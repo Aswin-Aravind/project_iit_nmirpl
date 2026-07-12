565f91042af2b14c5f667f3c55a2c606c01cf1bb
Aswin-Aravind <aswinaravind687@gmail.com>
2026-07-12 17:49:14 +0530

    refresh issue resolvation(first)

--- lab-website/src/main.jsx
+++ lab-website/src/main.jsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Force to start from homepage on fresh load / refresh
-if (window.location.pathname !== '/') {
-  window.history.replaceState(null, '', '/');
-}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
